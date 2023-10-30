96a87435b5e90ea4d30c34655d99328df734fa10
Valerio Orlandini <valeriorlandini@gmail.com>
2023-10-30 19:00:36 +0100

    Comb and allpass algorithms, to be fixed and used later

--- source/projects/include/comb.h
+++ source/projects/include/comb.h
@@ -20,34 +20,37 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************/
 
-#ifndef DELAY_H_
-#define DELAY_H_
+#ifndef COMB_H_
+#define COMB_H_
 
-#include <algorithm>
-#include <cmath>
-#include <vector>
 #include <iostream>
 
-#include "interp.h"
+#include "delay.h"
 
 template <class TSample>
-class Delay
+class Comb
 {
 public:
-    Delay(const TSample &sample_rate = 44100.0,
-          const TSample &max_delay_time = 5000.0,
-          const TSample &delay_time = 1000.0,
-          const TSample &feedback = 0.0);
+    Comb(const TSample &sample_rate = 44100.0,
+         const TSample &max_delay_time = 1000.0,
+         const TSample &delay_time = 1000.0,
+         const TSample &gain = 0.707,
+         const TSample &feedforward = 0.707,
+         const TSample &feedback = 0.707);
 
     bool set_sample_rate(const TSample &sample_rate);
     bool set_time(const TSample &delay_time);
     bool set_max_time(const TSample &max_delay_time, bool clear = true);
+    bool set_gain(const TSample &gain);
+    bool set_feedforward(const TSample &feedforward);
     bool set_feedback(const TSample &feedback);
     void clear();
 
     TSample get_time();
     int get_samples();
     TSample get_max_time();
+    TSample get_gain();
+    TSample get_feedforward();
     TSample get_feedback();
 
     inline TSample run(const TSample &input);
@@ -56,26 +59,23 @@ public:
     inline TSample get_last_sample();
 
 private:
-    TSample max_delay_time_;
     TSample sample_rate_;
+    TSample max_delay_time_;
     TSample delay_time_;
-
-    int delay_samples_[2];
-    TSample delay_interp_;
-
-    TSample feedback_;
-
     TSample output_;
 
-    int read_pos_[2];
-    int write_pos_;
+    TSample gain_;
+    TSample feedforward_;
+    TSample feedback_;
 
-    std::vector<TSample> buffer_;
+    Delay<TSample> ff_delay_;
+    Delay<TSample> fb_delay_;
 };
 
 template <class TSample>
-Delay<TSample>::Delay(const TSample &sample_rate, const TSample &max_delay_time,
-                      const TSample &delay_time, const TSample &feedback)
+Comb<TSample>::Comb(const TSample &sample_rate, const TSample &max_delay_time,
+                    const TSample &delay_time, const TSample &gain,
+                    const TSample &feedforward, const TSample &feedback)
 {
     if (sample_rate > 0.0)
     {
@@ -86,108 +86,115 @@ Delay<TSample>::Delay(const TSample &sample_rate, const TSample &max_delay_time,
         sample_rate_ = 44100.0;
     }
 
+    ff_delay_.set_sample_rate(sample_rate_);
+    fb_delay_.set_sample_rate(sample_rate_);
+
     if (max_delay_time > 0.0)
     {
         max_delay_time_ = max_delay_time;
     }
     else
     {
-        max_delay_time_ = 5000.0;
+        max_delay_time_ = 1000.0;
     }
 
-    buffer_.assign((unsigned int)ceil(max_delay_time_ * sample_rate_ * 0.001) + 1, 0.0);
-
-    write_pos_ = 0;
+    ff_delay_.set_max_time(max_delay_time_);
+    fb_delay_.set_max_time(max_delay_time_);
 
     if (delay_time <= max_delay_time_)
     {
-        set_time(delay_time);
+        delay_time_ = delay_time;
     }
     else
     {
-        set_time(max_delay_time_);
+        delay_time_ = max_delay_time_;
     }
 
+    ff_delay_.set_time(delay_time_);
+    fb_delay_.set_time(delay_time_);
+
+    set_gain(gain);
+    set_feedforward(feedforward);
     set_feedback(feedback);
 }
 
 template <class TSample>
-bool Delay<TSample>::set_sample_rate(const TSample &sample_rate)
+bool Comb<TSample>::set_sample_rate(const TSample &sample_rate)
 {
     if (sample_rate > 0.0)
     {
         sample_rate_ = sample_rate;
 
-        set_max_time(max_delay_time_);
-        set_time(delay_time_);
+        ff_delay_.set_sample_rate(sample_rate_);
+        fb_delay_.set_sample_rate(sample_rate_);
 
         return true;
     }
 
-    std::cerr << "Delay::sample_rate_ must be greater than zero\n";
+    std::cerr << "Comb::sample_rate_ must be greater than zero\n";
 
     return false;
 }
 
 template <class TSample>
-bool Delay<TSample>::set_max_time(const TSample &max_delay_time, bool clear)
+bool Comb<TSample>::set_max_time(const TSample &max_delay_time, bool clear)
 {
     if (max_delay_time > 0.0)
     {
         max_delay_time_ = max_delay_time;
 
-        if (clear)
-        {
-            buffer_.assign((unsigned int)ceil(max_delay_time_ * sample_rate_ * 0.001) + 1, 0.0);
-        }
-        else
-        {
-            buffer_.resize((unsigned int)ceil(max_delay_time_ * sample_rate_ * 0.001) + 1, 0.0);
-        }
+        ff_delay_.set_max_time(max_delay_time_, clear);
+        fb_delay_.set_max_time(max_delay_time_, clear);
 
         if (delay_time_ > max_delay_time_)
         {
-            set_delay_time(max_delay_time_);
+            delay_time_ = max_delay_time_;
         }
 
         return true;
     }
 
-    std::cerr << "Delay::max_delay_time_ must be greater than zero\n";
+    std::cerr << "Comb::max_delay_time_ must be greater than zero\n";
 
     return false;
 }
 
 template <class TSample>
-bool Delay<TSample>::set_time(const TSample &delay_time)
+bool Comb<TSample>::set_time(const TSample &delay_time)
 {
     if (delay_time >= 0.0 && delay_time <= max_delay_time_)
     {
         delay_time_ = delay_time;
 
-        delay_samples_[0] = (int)floor(sample_rate_ * delay_time_ * 0.001);
-        delay_samples_[1] = (int)ceil(sample_rate_ * delay_time_ * 0.001) % buffer_.size();
-        delay_interp_ = (sample_rate_ * delay_time_ * 0.001) - floor(sample_rate_ * delay_time_ * 0.001);
-
-        for (int p = 0; p <= 1; p++)
-        {
-            read_pos_[p] = write_pos_ - delay_samples_[p];
-            if (read_pos_[p] < 0)
-            {
-                read_pos_[p] = buffer_.size() + read_pos_[p];
-            }
-        }
+        ff_delay_.set_time(delay_time_);
+        fb_delay_.set_time(delay_time_);
 
         return true;
     }
 
-    std::cerr << "Delay::delay_time_ must be inside [0.0, Delay::max_delay_time_] range\n";
+    std::cerr << "Comb::delay_time_ must be inside [0.0, Comb::max_delay_time_] range\n";
 
     return false;
 }
 
 template <class TSample>
-bool Delay<TSample>::set_feedback(const TSample &feedback)
+bool Comb<TSample>::set_gain(const TSample &gain)
+{
+    gain_ = gain;
+
+    return true;
+}
+
+template <class TSample>
+bool Comb<TSample>::set_feedforward(const TSample &feedforward)
+{
+    feedforward_ = feedforward;
+
+    return true;
+}
+
+template <class TSample>
+bool Comb<TSample>::set_feedback(const TSample &feedback)
 {
     feedback_ = feedback;
 
@@ -195,69 +202,64 @@ bool Delay<TSample>::set_feedback(const TSample &feedback)
 }
 
 template <class TSample>
-TSample Delay<TSample>::get_time()
+TSample Comb<TSample>::get_time()
 {
     return delay_time_;
 }
 
 template <class TSample>
-int Delay<TSample>::get_samples()
-{
-    return delay_samples_;
-}
-
-template <class TSample>
-TSample Delay<TSample>::get_max_time()
+TSample Comb<TSample>::get_max_time()
 {
     return max_delay_time_;
 }
 
 template <class TSample>
-TSample Delay<TSample>::get_feedback()
+TSample Comb<TSample>::get_gain()
+{
+    return gain_;
+}
+
+template <class TSample>
+TSample Comb<TSample>::get_feedforward()
 {
     return feedback_;
 }
 
 template <class TSample>
-void Delay<TSample>::clear()
+TSample Comb<TSample>::get_feedback()
 {
-    buffer_.assign(buffer_.size(), 0.0);
+    return feedback_;
 }
 
 template <class TSample>
-inline TSample Delay<TSample>::run(const TSample &input)
+void Comb<TSample>::clear()
 {
-    output_ = cosip(buffer_.at(read_pos_[0]), buffer_.at(read_pos_[1]), delay_interp_);
+    ff_delay_.clear();
+    fb_delay_.clear();
+}
 
-    buffer_.at(write_pos_) = input + (output_ * feedback_);
-
-    if (++write_pos_ >= buffer_.size())
-    {
-        write_pos_ = 0;
-    }
-
-    for (int p = 0; p <= 1; p++)
-    {
-        if (++read_pos_[p] >= buffer_.size())
-        {
-            read_pos_[p] = 0;
-        }
-    }
+template <class TSample>
+inline TSample Comb<TSample>::run(const TSample &input)
+{
+    TSample out = gain_ * input;
+    out += feedforward_ * ff_delay_.run(input);
+    out += feedback_ * fb_delay_.run(output_);
+    output_ = out;
 
     return output_;
 }
 
 template <class TSample>
-inline void Delay<TSample>::run(const TSample &input, TSample &output)
+inline void Comb<TSample>::run(const TSample &input, TSample &output)
 {
     output_ = run(input);
     output = output_;
 }
 
 template <class TSample>
-inline TSample Delay<TSample>::get_last_sample()
+inline TSample Comb<TSample>::get_last_sample()
 {
     return output_;
 }
 
-#endif // DELAY_H_
+#endif // COMB_H_
