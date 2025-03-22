1f165dd95f44ab8a07823732929201b05ddd4587
Valerio Orlandini <valeriorlandini@gmail.com>
2025-03-22 11:29:41 +0100

    Hopefully final fixes to crossloop~

--- source/projects/sonus.crossloop_tilde/sonus.crossloop_tilde.cpp
+++ source/projects/sonus.crossloop_tilde/sonus.crossloop_tilde.cpp
@@ -30,6 +30,7 @@ public:
         this,
         MIN_FUNCTION
         {
+            initialized_ = false;
             return {};
         }
     };
@@ -41,7 +42,7 @@ public:
         "buffer~ to work on."
     };
 
-	argument<int> channels_arg
+    argument<int> channels_arg
     {
         this,
         "outputs",
@@ -78,23 +79,26 @@ public:
         description {"Loop end in milliseconds."}
     };
 
-    message<> m_number {
+    message<> m_number
+    {
         this,
         "number",
         "Set the crossfade amount or loop start or end time, according to the inlet",
         MIN_FUNCTION {
-            switch (inlet) {
-                case 0:
-                    crossfade = number(args[0]);
-                    break;
-                case 1:
-                    target_loop_start = number(args[0]);
-                    loop_points_changed = true;
-                    break;
-                case 2:
-                    target_loop_end = number(args[0]);
-                    loop_points_changed = true;
-                    break;
+            switch (inlet)
+            {
+            case 0:
+                crossfade = number(args[0]);
+                loop_points_changed = true;
+                break;
+            case 1:
+                target_loop_start = number(args[0]);
+                loop_points_changed = true;
+                break;
+            case 2:
+                target_loop_end = number(args[0]);
+                loop_points_changed = true;
+                break;
             }
             return {};
         }
@@ -143,152 +147,169 @@ public:
                 }
             }
         }
-
-        if (loop_points_changed.load())
+        else
         {
-            loop_start = target_loop_start.load();
-            loop_end = target_loop_end.load();
-
-            sample_start = std::floor(loop_start * b.samplerate() * 0.001);
-            sample_start = std::clamp(sample_start, 0.0, static_cast<sample>(b.frame_count() - 1));
-
-            sample_end = static_cast<sample>(b.frame_count() - 1);
-            if (loop_end > 0.0)
+            if (loop_points_changed.load() || !initialized_)
             {
-                sample_end = std::floor(loop_end * b.samplerate() * 0.001);
-                sample_end = std::clamp(sample_end, 0.0, static_cast<sample>(b.frame_count() - 1));
-            }
+                initialized_ = true;
+                loop_start = target_loop_start.load();
+                loop_end = target_loop_end.load();
 
-            if (sample_start > sample_end)
-            {
-                std::swap(sample_start, sample_end);
-                direction_ = -1.0;
-            }
-            else
-            {
-                direction_ = 1.0;
-            }
+                sample_start = std::floor(loop_start * b.samplerate() * 0.001);
+                sample_start = std::clamp(sample_start, 0.0, static_cast<sample>(b.frame_count() - 1));
 
-            length = sample_end - sample_start;
-
-            crossfade_samples = std::max(1.0, crossfade * 0.005 * length);
-            crossfade_start_sample = sample_end - crossfade_samples;
-            crossfade_start_sample_reversed = sample_start + crossfade_samples;
-
-            playhead_ = std::clamp(playhead_, sample_start, sample_end);
-
-            loop_points_changed = false;
-        }
-
-        for (auto i = 0; i < input.frame_count(); ++i)
-        {
-            sample step = input.samples(0)[i] * direction_;
-            playhead_ += step;
-
-            if (step < 0.0)
-            {
-                if (playhead_ < sample_start)
+                sample_end = static_cast<sample>(b.frame_count() - 1);
+                if (loop_end > 0.0)
                 {
-                    playhead_ = sample_end - std::fmod(sample_start - playhead_, length) - crossfade_samples;
+                    sample_end = std::floor(loop_end * b.samplerate() * 0.001);
+                    sample_end = std::clamp(sample_end, 0.0, static_cast<sample>(b.frame_count() - 1));
                 }
-            }
-            else
-            {   
-                if (playhead_ > sample_end)
+
+                if (sample_start > sample_end)
                 {
-                    playhead_ = sample_start + std::fmod(playhead_ - sample_start, length) + crossfade_samples;
+                    std::swap(sample_start, sample_end);
+                    direction_ = -1.0;
                 }
+                else
+                {
+                    direction_ = 1.0;
+                }
+
+                length = sample_end - sample_start;
+
+                crossfade_samples = std::clamp(crossfade * 0.005 * length, 0.0, length * 0.5);
+                crossfade_start_sample = sample_end - crossfade_samples;
+                crossfade_start_sample_reversed = sample_start + crossfade_samples;
+
+                playhead_ = std::clamp(playhead_, sample_start, sample_end);
+
+                loop_points_changed = false;
             }
 
-            for (auto ch = 0; ch < outputs_ - 1; ch++)
+            if (length > 0.0)
             {
-                output.samples(ch)[i] = 0.0;
-
-                if (ch < b.channel_count())
+                for (auto i = 0; i < input.frame_count(); ++i)
                 {
-                    if (playhead_ > crossfade_start_sample && crossfade > 0.0 && step > 0.0)
+                    sample step = input.samples(0)[i] * direction_;
+                    playhead_ += step;
+
+                    if (step < 0.0)
                     {
-                        sample fade_in_gain = (playhead_ - crossfade_start_sample) / crossfade_samples;
-                        sample fade_out_gain = 1.0 - fade_in_gain;
-                        fade_in_gain = std::sqrt(fade_in_gain);
-                        fade_out_gain = std::sqrt(fade_out_gain);
-
-                        sample ceil = std::ceil(playhead_);
-                        if (ceil >= sample_end)
+                        if (playhead_ < sample_start)
                         {
-                            ceil = 0.0;
+                            playhead_ = sample_end - std::fmod(sample_start - playhead_, length) - crossfade_samples;
                         }
-                        sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
-                        sample floor;
-                        sample t = std::modf(playhead_, &floor);
-                        sample as = b.lookup(static_cast<unsigned int>(floor), ch);
-                        sample out = cosip(as, bs, t) * fade_out_gain;
-
-                        sample corresponding_ph = sample_start + playhead_ - crossfade_start_sample;
-
-                        ceil = std::ceil(corresponding_ph);
-                        if (ceil >= sample_end)
-                        {
-                            ceil = 0.0;
-                        }
-                        bs = b.lookup(static_cast<unsigned int>(ceil), ch);
-                        floor = 0.0;
-                        t = std::modf(corresponding_ph, &floor);
-                        as = b.lookup(static_cast<unsigned int>(floor), ch);
-                        out += cosip(as, bs, t) * fade_in_gain;
-
-                        output.samples(ch)[i] = out;
-                    }
-                    else if (playhead_ < crossfade_start_sample_reversed && crossfade > 0.0 && step < 0.0)
-                    {
-                        sample fade_in_gain = (crossfade_start_sample_reversed - playhead_) / crossfade_samples;
-                        sample fade_out_gain = 1.0 - fade_in_gain;
-                        fade_in_gain = std::sqrt(fade_in_gain);
-                        fade_out_gain = std::sqrt(fade_out_gain);
-
-                        sample ceil = std::ceil(playhead_);
-                        if (ceil >= sample_end)
-                        {
-                            ceil = 0.0;
-                        }
-                        sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
-                        sample floor = 0.0;
-                        sample t = std::modf(playhead_, &floor);
-                        sample as = b.lookup(static_cast<unsigned int>(floor), ch);
-                        sample out = cosip(as, bs, t) * fade_out_gain;
-
-                        sample corresponding_ph = sample_end - (crossfade_start_sample_reversed - playhead_);
-
-                        ceil = std::ceil(corresponding_ph);
-                        if (ceil >= sample_end)
-                        {
-                            ceil = 0.0;
-                        }
-                        bs = b.lookup(static_cast<unsigned int>(ceil), ch);
-                        floor;
-                        t = std::modf(corresponding_ph, &floor);
-                        as = b.lookup(static_cast<unsigned int>(floor), ch);
-                        out += cosip(as, bs, t) * fade_in_gain;
-
-                        output.samples(ch)[i] = out;
                     }
                     else
                     {
-                        sample ceil = std::ceil(playhead_);
-                        if (ceil >= sample_end)
+                        if (playhead_ > sample_end)
                         {
-                            ceil = 0.0;
+                            playhead_ = sample_start + std::fmod(playhead_ - sample_start, length) + crossfade_samples;
                         }
-                        sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
-                        sample floor;
-                        sample t = std::modf(playhead_, &floor);
-                        sample as = b.lookup(static_cast<unsigned int>(floor), ch);
+                    }
 
-                        output.samples(ch)[i] = cosip(as, bs, t);
+
+                    for (auto ch = 0; ch < outputs_ - 1; ch++)
+                    {
+                        output.samples(ch)[i] = 0.0;
+
+                        if (ch < b.channel_count())
+                        {
+                            if (playhead_ > crossfade_start_sample && crossfade > 0.0 && step > 0.0)
+                            {
+                                sample fade_in_gain = (playhead_ - crossfade_start_sample) / crossfade_samples;
+                                sample fade_out_gain = 1.0 - fade_in_gain;
+                                fade_in_gain = std::sqrt(fade_in_gain);
+                                fade_out_gain = std::sqrt(fade_out_gain);
+
+                                sample ceil = std::ceil(playhead_);
+                                if (ceil >= sample_end)
+                                {
+                                    ceil = 0.0;
+                                }
+                                sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
+                                sample floor;
+                                sample t = std::modf(playhead_, &floor);
+                                sample as = b.lookup(static_cast<unsigned int>(floor), ch);
+                                sample out = cosip(as, bs, t) * fade_out_gain;
+
+                                sample corresponding_ph = sample_start + playhead_ - crossfade_start_sample;
+
+                                ceil = std::ceil(corresponding_ph);
+                                if (ceil >= sample_end)
+                                {
+                                    ceil = 0.0;
+                                }
+                                bs = b.lookup(static_cast<unsigned int>(ceil), ch);
+                                floor = 0.0;
+                                t = std::modf(corresponding_ph, &floor);
+                                as = b.lookup(static_cast<unsigned int>(floor), ch);
+                                out += cosip(as, bs, t) * fade_in_gain;
+
+                                output.samples(ch)[i] = out;
+                            }
+                            else if (playhead_ < crossfade_start_sample_reversed && crossfade > 0.0 && step < 0.0)
+                            {
+                                sample fade_in_gain = (crossfade_start_sample_reversed - playhead_) / crossfade_samples;
+                                sample fade_out_gain = 1.0 - fade_in_gain;
+                                fade_in_gain = std::sqrt(fade_in_gain);
+                                fade_out_gain = std::sqrt(fade_out_gain);
+
+                                sample ceil = std::ceil(playhead_);
+                                if (ceil >= sample_end)
+                                {
+                                    ceil = 0.0;
+                                }
+                                sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
+                                sample floor = 0.0;
+                                sample t = std::modf(playhead_, &floor);
+                                sample as = b.lookup(static_cast<unsigned int>(floor), ch);
+                                sample out = cosip(as, bs, t) * fade_out_gain;
+
+                                sample corresponding_ph = sample_end - (crossfade_start_sample_reversed - playhead_);
+
+                                ceil = std::ceil(corresponding_ph);
+                                if (ceil >= sample_end)
+                                {
+                                    ceil = 0.0;
+                                }
+                                bs = b.lookup(static_cast<unsigned int>(ceil), ch);
+                                floor;
+                                t = std::modf(corresponding_ph, &floor);
+                                as = b.lookup(static_cast<unsigned int>(floor), ch);
+                                out += cosip(as, bs, t) * fade_in_gain;
+
+                                output.samples(ch)[i] = out;
+                            }
+                            else
+                            {
+                                sample ceil = std::ceil(playhead_);
+                                if (ceil >= sample_end)
+                                {
+                                    ceil = 0.0;
+                                }
+                                sample bs = b.lookup(static_cast<unsigned int>(ceil), ch);
+                                sample floor;
+                                sample t = std::modf(playhead_, &floor);
+                                sample as = b.lookup(static_cast<unsigned int>(floor), ch);
+
+                                output.samples(ch)[i] = cosip(as, bs, t);
+                            }
+                        }
                     }
                 }
             }
-	    }
+            else
+            {
+                for (auto i = 0; i < input.frame_count(); ++i)
+                {
+                    for (auto ch = 0; ch < outputs_ - 1; ch++)
+                    {
+                        output.samples(ch)[i] = 0.0;
+                    }
+                }
+            }
+        }
     }
 
 private:
@@ -296,7 +317,6 @@ private:
     std::vector<std::unique_ptr<outlet<>>> m_outlets;
     sample playhead_{0.0};
     sample direction_{1.0};
-    //std::vector<sample> playhead_;
     std::atomic<bool> loop_points_changed{false};
     std::atomic<sample> target_loop_start{0.0};
     std::atomic<sample> target_loop_end{0.0};
@@ -306,6 +326,7 @@ private:
     sample crossfade_samples{0.0};
     sample crossfade_start_sample{0.0};
     sample crossfade_start_sample_reversed{0.0};
+    bool initialized_{false};
 };
 
 MIN_EXTERNAL(crossloop_tilde);
